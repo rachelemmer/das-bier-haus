e8e66c4c040a137a2702dcb12d17d056e61c28c9
rachelemmer <rachelaemmer@gmail.com>
2020-10-20 09:52:26 -0600

    add logo to and style nav bar

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Logo from './dasbierhaus.png'
+
 
 export default function Navigation() {
     return (
         <nav className="nav-bar">
-            <Link to="/">Home</Link>
-            <br></br>
-            <Link to='Services'>Services</Link>
-            <br></br>
-            <Link to='Menu'>Menu</Link>
-            <br></br>
-            <Link to='About'>About</Link>
-            <br></br>
-            <Link to='Contact'>Contact</Link>
+            <Link to="/"><img className="logo" src={Logo} alt="das bier haus logo with stein"></img></Link>
+            <Link to='Services' className="link">Services</Link>
+            <Link to='Menu' className="link">Menu</Link>
+            <Link to='About' className="link">About</Link>
+            <Link to='Contact' className="link">Contact</Link>
         </nav>
     )
 }
