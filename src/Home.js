2b2455ccca15bdbcb6909539a9abc03570c1e1da
rachelemmer <rachelaemmer@gmail.com>
2020-10-21 06:26:00 -0600

    fix nav styling and add landing page photo

--- src/Home.js
+++ src/Home.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Home() {
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div className="home-page-container">
+            
         </div>
     )
 }
